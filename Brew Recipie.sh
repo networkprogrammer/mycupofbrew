729b8e6273bbde6bdc0fa260be94d15bafef35de
Bobin Joseph <networkprogrammer@users.noreply.github.com>
2015-06-03 19:06:32 -0700

    Update Brew Recipie.sh

--- Brew Recipie.sh
+++ Brew Recipie.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ssh-keygen -t rsa
 #xcode-select --install
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 brew install iperf iperf3 python python3 wakeonlan curl wget
 brew cask install anki github onyx transmission atom google-chrome macdown pycharm-ce vlc filezilla google-drive vmware-fusion flux iterm2 nosleep spectacle wacom-bamboo-tablet cord
